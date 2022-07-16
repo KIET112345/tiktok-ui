5c1cb7fe716efaa73549d570248f27d053cd685f
Kiet112345 <vankietclbk@gmail.com>
2022-07-16 16:53:17 +0700

    router/layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaultLayOut } from '~/components/Layouts';
+import { HeaderOnly } from '~/components/Layouts';
+
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">React App</header>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        let Layout = DefaultLayOut;
+                        if (route.layout === null) {
+                            Layout = Fragment;
+                        } else if (route.layout === HeaderOnly) {
+                            Layout = HeaderOnly;
+                        }
+                        const Page = route.component;
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 

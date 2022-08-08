b2251f559f222129da69b3b840ba53c7adfc6283
Kiet112345 <vankietclbk@gmail.com>
2022-08-08 13:28:15 +0700

    header6

--- src/components/Layouts/DefaultLayout/Header/index.js
+++ src/components/Layouts/DefaultLayout/Header/index.js
@@ -4,13 +4,19 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
     faCircleQuestion,
     faCircleXmark,
+    faCloudUpload,
+    faCoins,
     faEllipsisVertical,
+    faGears,
     faGlobe,
     faKeyboard,
     faMagnifyingGlass,
+    faSignOut,
     faSpinner,
+    faUser,
 } from '@fortawesome/free-solid-svg-icons';
-import Tippy from '@tippyjs/react/headless';
+import Tippy from '@tippyjs/react';
+import HeadlesTippy from '@tippyjs/react/headless';
 import 'tippy.js/dist/tippy.css'; // optional
 
 import styles from './Header.module.scss';
@@ -36,6 +42,14 @@ const MENU_ITEMS = [
     { icon: <FontAwesomeIcon icon={faCircleQuestion} />, title: 'Feedback and help', to: './feedback' },
     { icon: <FontAwesomeIcon icon={faKeyboard} />, title: 'Keyboard and shortcut' },
 ];
+const currentUser = true;
+const userMenu = [
+    { icon: <FontAwesomeIcon icon={faUser} />, title: 'View profile', to: '/@hoa' },
+    { icon: <FontAwesomeIcon icon={faCoins} />, title: 'Get coins', to: './coins' },
+    { icon: <FontAwesomeIcon icon={faGears} />, title: 'Settings', to: './settings' },
+    ...MENU_ITEMS,
+    { icon: <FontAwesomeIcon icon={faSignOut} />, title: 'Log Out', to: './logout', separate: true },
+];
 function Header() {
     const [searchResults, setSearchResults] = useState([]);
     useEffect(() => {
@@ -50,7 +64,6 @@ function Header() {
         switch (menuItem.type) {
             case 'Language':
                 // handle
-                console.log('VO DAY');
                 break;
             default:
                 break;
@@ -62,7 +75,7 @@ function Header() {
                 <div className={cx('logo')}>
                     <img src={images.logo} alt="Tiktok"></img>
                 </div>
-                <Tippy
+                <HeadlesTippy
                     interactive={true}
                     visible={searchResults.length > 0}
                     render={(attrs) => (
@@ -87,14 +100,34 @@ function Header() {
                             <FontAwesomeIcon icon={faMagnifyingGlass} />
                         </button>
                     </div>
-                </Tippy>
+                </HeadlesTippy>
                 <div className={cx('actions')}>
-                    <Button text>Upload</Button>
-                    <Button primary>Log in</Button>
-                    <Menu items={MENU_ITEMS} onChange={handleOnchange}>
-                        <button className={cx('more-btn')}>
-                            <FontAwesomeIcon icon={faEllipsisVertical} />
-                        </button>
+                    {currentUser ? (
+                        <>
+                            <Tippy delay={[0, 200]} content="Upload videos" placement="bottom">
+                                <button className={cx('action-btn')}>
+                                    <FontAwesomeIcon icon={faCloudUpload}></FontAwesomeIcon>
+                                </button>
+                            </Tippy>
+                        </>
+                    ) : (
+                        <>
+                            <Button text>Upload</Button>
+                            <Button primary>Log in</Button>
+                        </>
+                    )}
+                    <Menu items={currentUser ? userMenu : MENU_ITEMS} onChange={handleOnchange}>
+                        {currentUser ? (
+                            <img
+                                className={cx('user-avatar')}
+                                src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                                alt="Nguyen Van A"
+                            />
+                        ) : (
+                            <button className={cx('more-btn')}>
+                                <FontAwesomeIcon icon={faEllipsisVertical} />
+                            </button>
+                        )}
                     </Menu>
                 </div>
             </div>
